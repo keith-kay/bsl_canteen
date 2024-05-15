f50e35033ceba7db85483bede82ed6b1e28d1160
keith-kay <keithrhova@gmail.com>
2024-05-15 23:33:54 +0300

    final commit

--- resources/views/admin/reports.blade.php
+++ resources/views/admin/reports.blade.php
@@ -7,13 +7,13 @@ Admin | Reports
 
 @section('report')
 <style>
-    .dataTables_wrapper .dataTables_filter {
-        margin-bottom: 20px;
-    }
+.dataTables_wrapper .dataTables_filter {
+    margin-bottom: 20px;
+}
 
-    #reports-table thead {
-        border-top: 1px solid #dee2e6 !important;
-    }
+#reports-table thead {
+    border-top: 1px solid #dee2e6 !important;
+}
 </style>
 <div class="col-lg-12">
     <div class="card">
@@ -54,12 +54,8 @@ Admin | Reports
                         <label class="form-check-label" for="tea">Tea</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input meal-type-checkbox" type="checkbox" id="lunch" value="Lunch">
-                        <label class="form-check-label" for="lunch">Lunch</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input meal-type-checkbox" type="checkbox" id="supper" value="Supper">
-                        <label class="form-check-label" for="supper">Supper</label>
+                        <input class="form-check-input meal-type-checkbox" type="checkbox" id="lunch" value="Meal">
+                        <label class="form-check-label" for="lunch">Meal</label>
                     </div>
                 </div>
             </div>
@@ -108,114 +104,114 @@ Admin | Reports
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.0/xlsx.full.min.js"></script>
 <script>
-    $(document).ready(function() {
-        var table = $('#reports-table').DataTable({
-            "paging": true,
-            "lengthChange": false,
-            "searching": true,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-            "responsive": true,
-            "pageLength": 10,
+$(document).ready(function() {
+    var table = $('#reports-table').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+        "pageLength": 10,
+    });
+
+    // Function to filter table based on date range and meal types
+    function applyFilters() {
+        var fromDate = $('#from_date').val();
+        var toDate = $('#to_date').val();
+        var selectedMealTypes = $('.meal-type-checkbox:checked').map(function() {
+            return this.value;
+        }).get();
+
+        table.draw(); // Redraw the table with new filters
+    }
+
+    // Filter button click event handler
+    $('#filter-btn').on('click', function() {
+        applyFilters();
+    });
+
+    // Reset button click event handler
+    $('#reset-btn').on('click', function() {
+        $('#from_date').val('');
+        $('#to_date').val('');
+        $('.meal-type-checkbox').prop('checked', false);
+        table.draw(); // Redraw the table to reset filters
+    });
+
+    $('#export-btn').on('click', function(event) {
+        event.preventDefault(); // Prevent the default action of the button
+        exportDataToExcel();
+    });
+
+    // Function to export data to Excel
+    function exportDataToExcel() {
+        // Initialize the DataTable
+        var table = $('#reports-table').DataTable();
+
+        // Get filtered data (visible rows)
+        var filteredData = [];
+        table.rows({
+            search: 'applied'
+        }).every(function() {
+            filteredData.push(this.data());
         });
 
-        // Function to filter table based on date range and meal types
-        function applyFilters() {
+        // Extract column headers from the DataTable
+        var columnHeaders = table.columns().header().toArray().map(function(header) {
+            return $(header).text().trim();
+        });
+
+        // Create export data array with column headers
+        var exportData = [columnHeaders];
+
+        // Iterate through filtered data and add to exportData array
+        filteredData.forEach(function(rowData) {
+            var rowDataTrimmed = rowData.map(function(cellData) {
+                return cellData.trim(); // Trim whitespace from each cell data
+            });
+            exportData.push(rowDataTrimmed);
+        });
+
+        // Create a worksheet with the extracted data
+        var ws = XLSX.utils.aoa_to_sheet(exportData);
+
+        // Create a workbook and add the worksheet
+        var wb = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, 'FilteredReportData');
+
+        // Save the workbook to an Excel file
+        XLSX.writeFile(wb, 'MealTicketsReport_data.xlsx');
+    }
+
+    // Apply custom filtering function
+    $.fn.dataTable.ext.search.push(
+        function(settings, data, dataIndex) {
+            var logDate = data[6]; // Assuming date is in 7th column (index 6)
+            var mealType = data[5]; // Assuming meal type is in 6th column (index 5)
             var fromDate = $('#from_date').val();
             var toDate = $('#to_date').val();
             var selectedMealTypes = $('.meal-type-checkbox:checked').map(function() {
                 return this.value;
             }).get();
 
-            table.draw(); // Redraw the table with new filters
-        }
-
-        // Filter button click event handler
-        $('#filter-btn').on('click', function() {
-            applyFilters();
-        });
-
-        // Reset button click event handler
-        $('#reset-btn').on('click', function() {
-            $('#from_date').val('');
-            $('#to_date').val('');
-            $('.meal-type-checkbox').prop('checked', false);
-            table.draw(); // Redraw the table to reset filters
-        });
-
-        $('#export-btn').on('click', function(event) {
-            event.preventDefault(); // Prevent the default action of the button
-            exportDataToExcel();
-        });
-
-        // Function to export data to Excel
-        function exportDataToExcel() {
-            // Initialize the DataTable
-            var table = $('#reports-table').DataTable();
-
-            // Get filtered data (visible rows)
-            var filteredData = [];
-            table.rows({
-                search: 'applied'
-            }).every(function() {
-                filteredData.push(this.data());
-            });
-
-            // Extract column headers from the DataTable
-            var columnHeaders = table.columns().header().toArray().map(function(header) {
-                return $(header).text().trim();
-            });
-
-            // Create export data array with column headers
-            var exportData = [columnHeaders];
-
-            // Iterate through filtered data and add to exportData array
-            filteredData.forEach(function(rowData) {
-                var rowDataTrimmed = rowData.map(function(cellData) {
-                    return cellData.trim(); // Trim whitespace from each cell data
-                });
-                exportData.push(rowDataTrimmed);
-            });
-
-            // Create a worksheet with the extracted data
-            var ws = XLSX.utils.aoa_to_sheet(exportData);
-
-            // Create a workbook and add the worksheet
-            var wb = XLSX.utils.book_new();
-            XLSX.utils.book_append_sheet(wb, ws, 'FilteredReportData');
-
-            // Save the workbook to an Excel file
-            XLSX.writeFile(wb, 'MealTicketsReport_data.xlsx');
-        }
-
-        // Apply custom filtering function
-        $.fn.dataTable.ext.search.push(
-            function(settings, data, dataIndex) {
-                var logDate = data[6]; // Assuming date is in 7th column (index 6)
-                var mealType = data[5]; // Assuming meal type is in 6th column (index 5)
-                var fromDate = $('#from_date').val();
-                var toDate = $('#to_date').val();
-                var selectedMealTypes = $('.meal-type-checkbox:checked').map(function() {
-                    return this.value;
-                }).get();
-
-                // Filter by date range
-                if ((fromDate !== '' && toDate !== '') && (logDate < fromDate || logDate > toDate)) {
-                    return false;
-                }
-
-                // Filter by meal types
-                if (selectedMealTypes.length > 0 && !selectedMealTypes.includes(mealType)) {
-                    return false;
-                }
-
-                return true;
+            // Filter by date range
+            if ((fromDate !== '' && toDate !== '') && (logDate < fromDate || logDate > toDate)) {
+                return false;
             }
-        );
 
-        // Hide DataTable search input
-        $('.dataTables_filter').hide();
-    });
+            // Filter by meal types
+            if (selectedMealTypes.length > 0 && !selectedMealTypes.includes(mealType)) {
+                return false;
+            }
+
+            return true;
+        }
+    );
+
+    // Hide DataTable search input
+    $('.dataTables_filter').hide();
+});
 </script>
 @stop
