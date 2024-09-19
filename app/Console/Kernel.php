f9e5b0af9e519eb8961661c3040fd9da41f0297a
keith-kay <80747944+keith-kay@users.noreply.github.com>
2024-09-19 15:45:16 +0300

    Add files via upload

---
+++ app/Console/Kernel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Register the commands for the application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        // Register your custom Artisan commands here
+        \App\Console\Commands\GenerateUserPins::class, // Add your command here
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // Schedule your commands if necessary
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
